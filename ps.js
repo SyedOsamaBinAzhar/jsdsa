2b613a668df2135b4b00bfd11515a93f6b2bd35e
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2023-01-22 13:40:12 +0500

    question3

--- ps.js
+++ ps.js
@@ -69,3 +69,46 @@
 // Space - O(1)
 
 // SOLUTION:
+// function areThereDuplicates(...elements) {
+//   let left = 0;
+//   let right = elements.length - 1;
+//   if (elements.includes(1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 || 0)){
+//     elements.sort()
+//   }
+//     while (left < right) {
+//       if (elements[left] === elements[right]) {
+//         // console.log("true here")
+//         return true;
+//       } else {
+//         left++;
+//         // right--;
+//       }
+//     }
+//   return false;
+// }
+
+// console.log(areThereDuplicates(1, 2, 2))
+// console.log(areThereDuplicates(1, 2, 3))
+// console.log(areThereDuplicates("a", "b", "c", "a"));
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// QUESTION:3
+// Multiple Pointers - averagePair
+// Write a function called averagePair. Given a sorted array of integers and a target average, determine 
+// if there is a pair of values in the array where the average of the pair equals the target average. 
+// There may be more than one pair that matches the average target.
+
+// Bonus Constraints:
+// Time: O(N)
+// Space: O(1)
+
+// Sample Input:
+// averagePair([1,2,3],2.5) // true
+// averagePair([1,3,3,5,6,7,10,12,19],8) // true
+// averagePair([-1,0,3,4,5,6], 4.1) // false
+// averagePair([],4) // false
+
+//SOLUTION:
+function averagePair(){
+    
+}
