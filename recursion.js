f8bb7bf6db00427a156bcaab20e0efda18c5e3f0
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2023-01-23 17:58:14 +0500

    pure  recursion

--- recursion.js
+++ recursion.js
@@ -31,25 +31,72 @@
 
 ///////////////////////////////////////////////////////////////////////////////////
 // FACTORIAL ITERATIVE
-// function factorial(num){
-//     let total = 1;
-//     for(let i = num; i > 1; i--){
-//         total *= i
-//     }
-//     return total;
-// }
-// factorial(7)
+function factorial(num){
+    let total = 1;
+    for(let i = num; i > 1; i--){
+        total *= i
+    }
+    return total;
+}
+factorial(7)
 
 
 
 //FACTORIAL RECURSIVE:
-// function factorial(num) {
-//   if (num === 1) return 1;
-//   // Explanation:
-//   //3 * factorial(2)
-//   //2 * factorial(1)
-//   //2 * 1
-//   return num * factorial(num - 1);
-// }
+function factorial(num) {
+  if (num === 1) return 1;
+  // Explanation:
+  //3 * factorial(2)
+  //2 * factorial(1)
+  //2 * 1
+  return num * factorial(num - 1);
+}
 
-// console.log(factorial(3));
+console.log(factorial(3));
+
+//HELPER METHOD RECURSION -> Design pattern
+function collectOddValues(arr){
+    
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length === 0) {
+            return;
+        }
+        
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+        
+        helper(helperInput.slice(1))
+    }
+    
+    helper(arr)
+
+    return result;
+}
+
+collectOddValues([1,2,3,4,5,6,7,8,9])
+
+
+function collectOddValues(arr){
+    let newArr = [];
+    
+    if(arr.length === 0) {
+        return newArr;
+    }
+        
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+        
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+collectOddValues([1,2,3,4,5])
+                                        
+                                                                
+                                                                             
+                            
+            
