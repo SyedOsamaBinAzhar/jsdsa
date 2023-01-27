6d410c1f526dcf3154f162eca23c18a11e496ff5
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2023-01-27 22:02:54 +0500

    binary search

--- search.js
+++ search.js
@@ -8,7 +8,7 @@
 //     return -1;
 //   }
 //   // add whatever parameters you deem necessary - good luck!
-  
+
 //   console.log(linearSearch([10, 15, 20, 25, 30], 10))
 
 // TIME COMPLEXITY:
@@ -16,3 +16,40 @@
 // Worst Case: O(N)
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// BINARY SEARCH:
+// Original Solution
+function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]){
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+    if(arr[middle] === elem){
+        return middle;
+    }
+    return -1;
+}
+
+// Refactored Version
+function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]) end = middle - 1;
+        else start = middle + 1;
+        middle = Math.floor((start + end) / 2);
+    }
+    return arr[middle] === elem ? middle : -1;
+}
+
+binarySearch([2,5,6,9,13,15,28,30], 103)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
