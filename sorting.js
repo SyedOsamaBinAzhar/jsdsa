a2b8ced000d9e4106ffd20e7a6defc1fdd5224b5
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2023-01-30 01:15:58 +0500

    merge sort

--- sorting.js
+++ sorting.js
@@ -101,7 +101,7 @@
 //   for (var i = 1; i < arr.length; i++) {
 //     currentVal = arr[i];
 //     for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-//       arr[j + 1] = arr[j]; 
+//       arr[j + 1] = arr[j];
 //       console.log(arr);
 //       console.log(j);
 //     }
@@ -119,3 +119,45 @@
 // Worst Case: O(Nsquare)
 // Best Case: O(Nsquare)
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// MERGE SORT:
+// Merges two already sorted arrays
+// function merge(arr1, arr2){
+//     let results = [];
+//     let i = 0;
+//     let j = 0;
+//     while(i < arr1.length && j < arr2.length){
+//         if(arr2[j] > arr1[i]){
+//             results.push(arr1[i]);
+//             i++;
+//         } else {
+//             results.push(arr2[j])
+//             j++;
+//         }
+//     }
+//     while(i < arr1.length) {
+//         results.push(arr1[i])
+//         i++;
+//     }
+//     while(j < arr2.length) {
+//         results.push(arr2[j])
+//         j++;
+//     }
+//     return results;
+// }
+
+// Recrusive Merge Sort
+// function mergeSort(arr){
+//     if(arr.length <= 1) return arr;
+//     let mid = Math.floor(arr.length/2);
+//     let left = mergeSort(arr.slice(0,mid));
+//     let right = mergeSort(arr.slice(mid));
+//     return merge(left, right);
+// }
+
+// mergeSort([10,24,76,73])
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
