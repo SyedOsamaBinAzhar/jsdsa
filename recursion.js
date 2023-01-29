6f43f6914e1966d4635aee3772c3ce1279815b94
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2023-01-29 23:19:30 +0500

    test

--- recursion.js
+++ recursion.js
@@ -151,14 +151,14 @@
 // productOfArray([1,2,3]) // 6
 // productOfArray([1,2,3,10]) // 60
 
-function productOfArray(arr) {
-  if (arr.length === 0) {
-    return 1;
-  }
-  return arr[0] * productOfArray(arr.slice(1));
-}
+// function productOfArray(arr) {
+//   if (arr.length === 0) {
+//     return 1;
+//   }
+//   return arr[0] * productOfArray(arr.slice(1));
+// }
 
-console.log(productOfArray([1,2,3,10]));
+// console.log(productOfArray([1,2,3,10]));
 
-// console.log([].length)
+console.log([].length)
 ///////////////////////////////////////////////////////////////////////////////////////////
