1dcb3f071903d938d5ec99f8699f6eb8b8127447
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2023-02-06 22:14:19 +0500

    test

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -1,34 +1,34 @@
-node of a linked list
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null;
-    }
-}
+// node of a linked list
+// class Node{
+//     constructor(val){
+//         this.val = val;
+//         this.next = null;
+//     }
+// }
 
-class SinglyLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-    push(val){
-        var newNode = new Node(val);
-        if(!this.head){
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.head.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
-    }
-}
+// class SinglyLinkedList{
+//     constructor(){
+//         this.head = null;
+//         this.tail = null;
+//         this.length = 0;
+//     }
+//     push(val){
+//         var newNode = new Node(val);
+//         if(!this.head){
+//             this.head = newNode;
+//             this.tail = this.head;
+//         } else {
+//             this.head.next = newNode;
+//             this.tail = newNode;
+//         }
+//         this.length++;
+//         return this;
+//     }
+// }
 
-var list = new SinglyLinkedList()
-console.log(list)
-console.log(list.push("HELLO"))
-console.log(list.push("WORLD"))
+// var list = new SinglyLinkedList()
+// console.log(list)
+// console.log(list.push("HELLO"))
+// console.log(list.push("WORLD"))
 
-list.push("GOODBYE")
+// list.push("GOODBYE")
